6c866cf2aa4e5c203bb92793bd0a1e4572a169ef
Yutong Zhao <proteneer@gmail.com>
2018-01-12 14:01:34 -0500

    Featurization rewrite.

---
+++ khan/model/symmetrizer.py
@@ -0,0 +1,221 @@
+import tensorflow as tf
+import numpy as np
+
+
+class Symmetrizer():
+
+    def __init__(self,
+        R_Rs=(0.5, 0.7, 1.0, 1.5), # rename to R_Rs, A_Rs, etc.
+        R_Rc=16,
+        R_eta=0.3,
+        A_Rs=(0.2, 0.25, 1,1,1,1,1,1,1),
+        A_Rc=8,
+        A_eta=0.3,
+        A_thetas=(0.2, 0.4, 0.5, 0.6),
+        A_zeta=0.01):
+        """
+        Symmetrizer implements the ANI symmetry functions as described in doi:10.1039/C6SC05720A
+
+        Parameters
+        ----------
+        R_Rs: array-like of floats
+            Elements corresponding to radial spikes
+
+        R_Rc: float
+            Cutoff for the radial part of featurization
+
+        R_eta: float
+            Eta parameter for the radial part of featurization
+
+        A_Rs: array-like of floats
+            Elements corresponding to angular spikes
+
+        A_Rc: float
+            Cutoff for the angular part of featurization
+
+        A_eta: float
+            Eta parameter for the angular part of featurization
+
+        A_theta: array-like of floats
+            Parameters determining the angular separation
+
+        A_zeta: float
+            Angular exponent (typically used to normalize to the radial parts)
+
+        """
+
+        self.max_atom_types = 4 # CNOH
+
+        self.R_Rs = np.array(R_Rs)
+        self.R_Rc = R_Rc
+        self.R_eta = R_eta
+
+        self.A_Rs = np.array(A_Rs)
+        self.A_Rc = A_Rc
+        self.A_eta = A_eta
+        self.A_thetas = np.array(A_thetas)
+        self.A_zeta = A_zeta
+
+        self.gdb_sess = tf.Session()
+
+    def radial_symmetry(self, atom_matrix):
+        """
+        Generate radial basis functions given an atom_matrix consisting of the atom types
+        and coordinates.
+
+        Parameters
+        ----------
+        atom_matrix: tf.Tensor
+            An atom matrix of shape (None, 4), where rank 0 determines the number of atoms
+            and rank 1 consists of (t, x, y, z) such that t is a compacted atomic number.
+
+        Returns
+        -------
+        tf.Tensor
+            Returns a tensor of shape (num_atoms, max_atom_types * len(self.Rs))
+
+        """
+        num_atoms = tf.shape(atom_matrix)[0]
+        atom_types = tf.cast(atom_matrix[:, 0], dtype=tf.int32)
+        atom_coords = atom_matrix[:, 1:]
+        type_groups = tf.dynamic_partition(atom_coords, atom_types, self.max_atom_types)
+        atom_coords = tf.expand_dims(atom_coords, axis=1) #  we reshape atom_coords to (num_atoms, 1, 3)
+        # loop through each of the atom types, eg. find all the atoms of type H, C, N, O, etc.
+        radial_features = []
+        for grp_pos in type_groups:
+            # grp_stack shape: (num_atoms, ?, 3)
+            # grp_pos shape: (grp_atoms, 3) 
+            grp_pos = tf.expand_dims(grp_pos, axis=0)            
+            R_ij = tf.norm(atom_coords - grp_pos, axis=2)
+
+            # R_ij shape: (num_atoms, ?), where ? is the number of matching atoms
+
+            f_C_true = 0.5*tf.cos(tf.div(np.pi * R_ij, self.R_Rc)) + 0.5
+            f_C_flags = tf.nn.relu(tf.sign(self.R_Rc - R_ij)) # 1 if within cutoff, 0 otherwise
+            f_C = f_C_true * f_C_flags
+
+            f_C = tf.expand_dims(f_C, 2)
+
+            delta_Rs = tf.expand_dims(R_ij, axis=-1) -  tf.reshape(self.R_Rs, (1, 1, -1))
+            # this is equivalent to the following:
+            # delta_Rs = tf.stack([R_ij] * len(self.R_Rs), axis=2) - self.R_Rs
+
+            summand = tf.exp(-self.R_eta*tf.pow(delta_Rs, 2)) * f_C
+
+            # summand is of shape (num_atoms, group_count, len(self.Rs))
+            # (ytz): a trick to determine if R_ij is such that i == j, we make the if and only if
+            # assumption that R_ij < 1e-6. 
+            R_ij_flags = tf.abs(R_ij) # deals with numerical inprecisions in cases of -1e6
+            R_ij_flags = tf.nn.relu(tf.sign(R_ij_flags - 1e-6))
+            R_ij_flags = tf.expand_dims(R_ij_flags, 2)
+
+            radial_features.append(tf.reduce_sum(summand * R_ij_flags, axis=1))
+
+        radial_features = tf.concat(radial_features, axis=1)
+        radial_features = tf.reshape(radial_features, (num_atoms, -1)) # ravel
+
+        return radial_features
+
+    def angular_symmetry(self, atom_matrix):
+        """
+        Generate radial basis functions given an atom_matrix consisting of the atom types
+        and coordinates.
+
+        Parameters
+        ----------
+        atom_matrix: tf.Tensor
+            An atom matrix of shape (None, 4), where rank 0 determines the number of atoms
+            and rank 1 consists of (t, x, y, z) such that t is a compacted atomic number.
+
+        Returns
+        -------        
+
+        """
+        num_atoms = tf.shape(atom_matrix)[0]
+        atom_idxs = tf.range(tf.shape(atom_matrix)[0])
+        atom_types = tf.cast(atom_matrix[:, 0], dtype=tf.int32)
+        atom_coords = atom_matrix[:, 1:] # atom_coords shape: (num_atoms, 3)
+        type_groups_idxs = tf.dynamic_partition(atom_idxs, atom_types, self.max_atom_types)
+        lookup = np.array([[[0, 3]],[[2,3]],[[5,3]]])
+
+        angular_features = []
+        for type_a in range(self.max_atom_types):
+            j_idxs = type_groups_idxs[type_a]
+
+            for type_b in range(type_a, self.max_atom_types):
+                k_idxs = type_groups_idxs[type_b]
+
+                tile_a = tf.tile(tf.expand_dims(j_idxs, 1), [1, tf.shape(k_idxs)[0]])  
+                tile_a = tf.expand_dims(tile_a, 2) 
+                tile_b = tf.tile(tf.expand_dims(k_idxs, 0), [tf.shape(j_idxs)[0], 1]) 
+                tile_b = tf.expand_dims(tile_b, 2) 
+                cartesian_product = tf.concat([tile_a, tile_b], axis=2) # int64s?
+                
+                group_coords = tf.nn.embedding_lookup(atom_coords, cartesian_product) # shape: (len(type_a), len(type_b), 2, 3)
+                delta_jk = group_coords[:, :, 0, :] - group_coords[:, :, 1, :]
+                R_jk = tf.norm(delta_jk, axis=-1)
+
+                dist_vec = tf.reshape(atom_coords, (-1, 1, 1, 1, 3)) # shape (6, 3, 3, 2, 3), vector difference
+
+                deltas = group_coords - dist_vec # shape: (num_atoms, len(type_a), len(type_b), 2, 3)
+                delta_ij = deltas[:, :, :, 0, :]
+                delta_ik = deltas[:, :, :, 1, :]
+
+                # LHS computation
+                denom = tf.multiply(tf.norm(delta_ij, axis=-1), (tf.norm(delta_ik, axis=-1))) #
+                dot = tf.reduce_sum(tf.multiply(delta_ij, delta_ik), axis=-1)
+
+                theta_ijk = tf.acos(dot / denom)   # if i=j || j=k then NaN
+
+                lhs = tf.pow(1 + tf.cos(tf.expand_dims(theta_ijk, -1) - tf.reshape(self.A_thetas, (1, 1, 1, -1))), self.A_zeta)
+                lhs = tf.where(tf.is_nan(lhs), tf.zeros_like(lhs), lhs) # clean up nans numerically, the real zeroing happens later
+                lhs = tf.where(tf.is_inf(lhs), tf.zeros_like(lhs), lhs) # clean up infs numerically, the real zeroing happens later
+                
+                # RHS computation
+                R_ij_ik = tf.norm(deltas, axis=-1) # shape (6, 3, 3, 2), norm distance
+                f_C_true = 0.5*tf.cos(tf.div(np.pi * R_ij_ik, self.A_Rc)) + 0.5 # TODO: refactor with radial code?
+                f_C_flags = tf.nn.relu(tf.sign(self.A_Rc - R_ij_ik)) # 1 if within cutoff, 0 otherwise
+                f_C_R_ij_ik  = f_C_true * f_C_flags
+
+                # note: element wise multiply
+                fCRi_fCRj = tf.multiply(f_C_R_ij_ik[:, :, :, 0], f_C_R_ij_ik[:, :, :, 1])
+                R_ij = R_ij_ik[:, :, :, 0]
+                R_ik = R_ij_ik[:, :, :, 1]
+
+                inner = tf.expand_dims((R_ij + R_ik) / 2.0, -1) - tf.reshape(self.A_Rs, (1, 1, 1, -1))
+                rhs = tf.exp(-self.A_eta*tf.pow(inner, 2)) * tf.expand_dims(fCRi_fCRj, -1)
+
+                # lhs shape: [num_atoms, len(type_a), len(type_b), len(A_thetas)]
+                # rhs shape: [num_atoms, len(type_a), len(type_b), len(A_Rs)]
+                lhs = tf.expand_dims(lhs, axis=3)
+                rhs = tf.expand_dims(rhs, axis=4)
+                summand = tf.multiply(lhs, rhs) # (num_atoms, len(type_a), len(type_b), len(A_Rs), len(A_thetas))
+
+                # zero-out/fix summand elements where i == j || j == k || i == k
+                # we store a triplet of shape
+                # (num_atoms, len(type_a), len(type_b), 3) where 3 is the distance of ij, ik, and jk respectively
+                # R_ij shape: (num_atoms, len(type_a), len(type_b))
+                # R_ik shape: (num_atoms, len(type_a), len(type_b))
+                R_jk = tf.tile(tf.expand_dims(R_jk, axis=0), [num_atoms, 1, 1])
+
+                R_ijk = tf.stack([R_ij, R_ik, R_jk], axis=-1)
+
+                # R_jk shape: (len(type_a), len(type_b))
+                # We want to form R_ijk of shape (num_atoms, len(type_a), len(type_b), 3)
+                min_dists = tf.reduce_min(R_ijk, axis=-1)
+                keep_flags = tf.nn.relu(tf.sign(tf.abs(min_dists) - 1e-7))
+
+                keep_flags = tf.expand_dims(keep_flags, -1)
+                keep_flags = tf.expand_dims(keep_flags, -1)
+
+
+                summand = tf.multiply(summand, keep_flags)
+                result = tf.multiply(tf.pow(np.float64(2.0), 1-self.A_zeta), tf.reduce_sum(summand, [1,2])) 
+                result = tf.reshape(result, (num_atoms, -1))
+
+                angular_features.append(result)
+
+        angular_features = tf.concat(angular_features, axis=1)
+        angular_features = tf.reshape(angular_features, (num_atoms, -1)) # ravel
+
+        return angular_features
---
+++ khan/model/test_symmetrizer.py
@@ -0,0 +1,163 @@
+import unittest
+import math
+import tensorflow as tf
+import numpy as np
+
+from khan.model import symmetrizer
+
+class TestSymmetrizer(unittest.TestCase):
+
+    def setUp(self):
+
+        self.sess = tf.Session()
+
+    def tearDown(self):
+
+        self.sess.close()
+
+    def test_radial_symmetry(self):
+
+        atom_matrix = np.array([
+            [0, 1.0, 2.0, 3.0], # H
+            [2, 2.0, 1.0, 4.0], # N
+            [0, 0.5, 1.2, 2.3], # H
+            [1, 0.3, 1.7, 3.2], # C
+            [2, 0.6, 1.2, 1.1], # N
+            [0, 14.0, 23.0, 15.0], # H
+        ])
+
+        coords_matrix = atom_matrix[:, 1:]
+
+        R_Rs = (0.5, 0.7)
+        R_Rc = 16
+        R_eta = 0.3
+
+        sym = symmetrizer.Symmetrizer(R_Rs=R_Rs, R_Rc=R_Rc, R_eta=R_eta)
+        feat = sym.radial_symmetry(tf.constant(atom_matrix))
+
+        result = self.sess.run(feat)
+
+        assert result.shape == (atom_matrix.shape[0], len(sym.R_Rs)*sym.max_atom_types)
+
+        # groupings
+        # 0
+        # 2   1
+        # 5 3 4 0
+        # H C N O
+
+        # H block:
+        #     0 2 5
+        # 0.5 x   x
+        # 0.7 x   x
+
+        # test atom 0
+        r_01 = np.linalg.norm(coords_matrix[0] - coords_matrix[2])
+        r_05 = np.linalg.norm(coords_matrix[0] - coords_matrix[5]) # > Rc 16 cutoff
+
+        fC = 0.5 * math.cos(math.pi * r_01/R_Rc) + 0.5
+        expected_r_0 = math.exp(-R_eta*math.pow(r_01-R_Rs[0], 2)) * fC
+
+        np.testing.assert_almost_equal(expected_r_0, 0.8607852536605668)
+        np.testing.assert_almost_equal(result[0][0], expected_r_0)
+
+        fC = 0.5 * math.cos(math.pi * r_01/R_Rc) + 0.5
+        expected_r_1 = math.exp(-R_eta*math.pow(r_01-R_Rs[1], 2)) * fC
+
+        np.testing.assert_almost_equal(result[0][1], expected_r_1)
+
+        # test atom 1
+        r_10 = np.linalg.norm(coords_matrix[1] - coords_matrix[0])
+        r_11 = np.linalg.norm(coords_matrix[1] - coords_matrix[2])
+        r_15 = np.linalg.norm(coords_matrix[1] - coords_matrix[5]) # > R_Rc 16 cutoff
+
+        fC0 = 0.5 * math.cos(math.pi * r_10/R_Rc) + 0.5
+        expected_r_0 = math.exp(-R_eta*math.pow(r_10-R_Rs[0], 2)) * fC0
+        fC1 = 0.5 * math.cos(math.pi * r_11/R_Rc) + 0.5
+        expected_r_1 = math.exp(-R_eta*math.pow(r_11-R_Rs[0], 2)) * fC1
+
+        np.testing.assert_almost_equal(result[1][0], expected_r_0 + expected_r_1)
+
+        # N block:
+        #   1 4
+
+        # test atom 3, second Rs
+        r_31 = np.linalg.norm(coords_matrix[3] - coords_matrix[1])
+        r_34 = np.linalg.norm(coords_matrix[3] - coords_matrix[4])
+
+        fC_r31 = 0.5 * math.cos(math.pi * r_31/R_Rc) + 0.5
+        expected_r_31 = math.exp(-R_eta*math.pow(r_31-R_Rs[1], 2)) * fC_r31
+        fC_r34 = 0.5 * math.cos(math.pi * r_34/R_Rc) + 0.5
+        expected_r_34 = math.exp(-R_eta*math.pow(r_34-R_Rs[1], 2)) * fC_r34
+
+        # 012345 -> 5 corresponds to Rs1
+        # HHCCNN
+        np.testing.assert_almost_equal(result[3][5], expected_r_31 + expected_r_34)
+
+    def test_angular_symmetry(self):
+
+        atom_matrix = np.array([
+            [0, 1.0, 2.0, 3.0], # H
+            [2, 2.0, 1.0, 4.0], # N
+            [0, 0.5, 1.2, 2.3], # H
+            [1, 0.3, 1.7, 3.2], # C
+            [2, 0.6, 1.2, 1.1], # N
+            [0, 14.0, 23.0, 15.0], # H
+            [0, 2.0, 0.5, 0.3], # H
+            [0, 2.3, 0.2, 0.4], # H
+        ])
+
+        coords_matrix = atom_matrix[:, 1:]
+
+        A_Rs = (0.3, 0.5)
+        A_Rc = 16
+        A_eta = 0.3
+        A_zeta = 0.01
+        A_thetas = (0.1, 0.11)
+
+        sym = symmetrizer.Symmetrizer(A_Rs=A_Rs, A_Rc=A_Rc, A_eta=A_eta, A_zeta=A_zeta, A_thetas=A_thetas)
+        angular_feat = sym.angular_symmetry(tf.constant(atom_matrix))
+
+        obtained = self.sess.run(angular_feat)
+
+        assert np.isfinite(obtained).all()
+
+        assert obtained.shape == (atom_matrix.shape[0], len(sym.A_Rs)*len(sym.A_thetas)*sym.max_atom_types*(sym.max_atom_types+1)/2)
+
+        # print(result.shape)
+        # print(result)
+
+        # compute 0-HH features
+
+        # for atom 2, which is an H
+        #                                   x  x
+        # the HH feature should include (2[05,50,06,60,07,70]), but 5-x distance is outside of the shell. So the only contribution should be
+        # from the 205.
+        pair_lists = [(0, 6), (6, 0), (0, 7), (7, 0), (6, 7), (7, 6)]
+
+        expected = np.zeros((2, 2))
+
+        for r_idx, Rs in enumerate(A_Rs):
+            for t_idx, theta in enumerate(A_thetas):
+                cum_sum = 0
+                for j, k in pair_lists:
+                    v_ij = coords_matrix[2] - coords_matrix[j]
+                    v_ik = coords_matrix[2] - coords_matrix[k]
+                    R_ij = np.linalg.norm(v_ij)
+                    R_ik = np.linalg.norm(v_ik)
+
+                    fC_Rij = 0.5 * math.cos(math.pi * R_ij/A_Rc) + 0.5
+                    fC_Rik = 0.5 * math.cos(math.pi * R_ik/A_Rc) + 0.5
+
+                    rhs = np.exp(-A_eta*np.power((R_ij + R_ik)/2 - Rs, 2.0)) * fC_Rij * fC_Rik
+                    theta_ijk = np.arccos(np.dot(v_ij, v_ik) / (R_ij * R_ik))
+                    lhs = np.power(1 + np.cos(theta_ijk - theta), A_zeta)
+
+                    cum_sum += lhs * rhs
+
+                expected[r_idx][t_idx] = cum_sum * np.power(2.0, 1 - A_zeta)
+
+        np.testing.assert_almost_equal(expected[0][0], obtained[2][0])
+        np.testing.assert_almost_equal(expected[0][1], obtained[2][1])
+        np.testing.assert_almost_equal(expected[1][0], obtained[2][2])
+        np.testing.assert_almost_equal(expected[1][1], obtained[2][3])
+
