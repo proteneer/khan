a3d72d11f13e8053afe613cd5ae8ad841045a150
Yutong Zhao <proteneer@gmail.com>
2018-04-04 15:14:39 -0400

    refactor

--- data_loaders.py
+++ data_loaders.py
@@ -60,12 +60,12 @@ class DataLoader():
             gdb_files = [
                 os.path.join(data_dir, "ani_gdb_s01.h5"),
                 os.path.join(data_dir, "ani_gdb_s02.h5"),
-                # os.path.join(data_dir, "ani_gdb_s03.h5"),
-                # os.path.join(data_dir, "ani_gdb_s04.h5"),
-                # os.path.join(data_dir, "ani_gdb_s05.h5"),
-                # os.path.join(data_dir, "ani_gdb_s06.h5"),
-                # os.path.join(data_dir, "ani_gdb_s07.h5"),
-                # os.path.join(data_dir, "ani_gdb_s08.h5"),
+                os.path.join(data_dir, "ani_gdb_s03.h5"),
+                os.path.join(data_dir, "ani_gdb_s04.h5"),
+                os.path.join(data_dir, "ani_gdb_s05.h5"),
+                os.path.join(data_dir, "ani_gdb_s06.h5"),
+                os.path.join(data_dir, "ani_gdb_s07.h5"),
+                os.path.join(data_dir, "ani_gdb_s08.h5"),
             ]
 
             # gdb_files = [os.path.join(data_dir, "ani_gdb_s03.h5")]
